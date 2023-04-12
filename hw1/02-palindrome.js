e7cedd8a8a9835c6d7adcd2f6f3160fe82610ba7
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-04-12 09:46:57 -0700

    code added to homework1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,27 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+output = document.getElementById("result")
+
+function handleInput() {
+
+	if (elem.value < 0) {
+		output.style.color = "red"
+		output.innerHTML = "Please Enter a possitive number!"
+	}
+
+	else {
+		let elem_str = elem.value.toString()
+		let reverse_elem = elem_str.split('').reverse().join('')
+		console.log(reverse_elem)
+		if (elem_str == reverse_elem) {
+			output.style.color = "green"
+			output.innerHTML = "Yes. This is Pallindrome!";
+		}
+		else {
+			output.style.color = "red"
+			output.innerHTML = "No. Try again.";
+		}
+	}
+
+}
