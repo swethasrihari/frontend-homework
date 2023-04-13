b81afcdf063d0ddc6f1aa63559f5210f6b782bf5
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-04-13 15:19:05 -0700

    updated: added input id in 02pallindrome.html,added named function expression for handleInput() and added bootstrap color classes to 02pallindrome.js

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,27 +1,27 @@
 const elem = document.querySelector('input');
 
-elem.addEventListener('input', handleInput);
-output = document.getElementById("result")
+output = document.getElementById("result");
 
-function handleInput() {
+const handleInput = function handleInput() {
 
 	if (elem.value < 0) {
-		output.style.color = "red"
-		output.innerHTML = "Please Enter a possitive number!"
+		output.className = 'text-danger';
+		output.innerHTML = "Please Enter a possitive number!";
 	}
 
 	else {
-		let elem_str = elem.value.toString()
-		let reverse_elem = elem_str.split('').reverse().join('')
-		console.log(reverse_elem)
-		if (elem_str == reverse_elem) {
-			output.style.color = "green"
-			output.innerHTML = "Yes. This is Pallindrome!";
+		let elem_str = elem.value.toString();
+		let reverse_elem = elem_str.split('').reverse().join('');
+		console.log(reverse_elem);
+		if (elem_str === reverse_elem) {
+            output.className = 'text-success';
+            output.innerHTML = "Yes. This is Pallindrome!";
 		}
 		else {
-			output.style.color = "red"
+            output.className = 'text-danger';
 			output.innerHTML = "No. Try again.";
 		}
 	}
 
 }
+elem.addEventListener('input', handleInput);
