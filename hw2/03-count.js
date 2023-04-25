0c697c40f4a388af472dd9d0a52013c1946c1288
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-04-25 16:20:47 -0700

    Files added to hw2

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,39 @@
 // Add your code here
+const searchWord = document.getElementById("search-word");
+const content = document.getElementById("content");
+const sentence = document.getElementById("content").innerHTML;
+const regex = new RegExp(searchWord, "gi");
 
-input.addEventListener('keydown', handleKeyDown);
+const findAndHighlight = function findAndHighlight() {
+  if (searchWord.value !== "") {
+    let words = content.innerHTML.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === searchWord.value) {
+        words[i] = words[i].replace(
+          /(<mark class="highlight">|<\/mark>)/gim,
+          ""
+        );
+        words[i] = words[i].replace(
+          words[i],
+          '<mark class="highlight">$&</mark>'
+        );
+        console.log("Highlighting");
+      }
+      console.log(searchWord.value);
+    }
+    content.innerHTML = words.join(" ");
+  } else {
+    content.innerHTML = sentence;
+  }
+};
+const enterKeyPressed = function enterKeyPressed(eventNo) {
+  if (eventNo.keyCode === 13) {
+    content.innerHTML = sentence;
+  }
+};
+const handleInput = function handleInput() {
+  console.log(searchWord.value);
+  findAndHighlight();
+};
+
+searchWord.addEventListener("input", handleInput);
--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,49 @@
 // Add your code here
+const button = document.getElementById("button");
+const time = document.getElementById("time");
+let setTimeInterval;
+let colorChange = true;
+let lbl = document.getElementById("myLabel");
+let warningLabel = false;
+const randomRGB = function generateRandomNumber() {
+  return Math.floor(Math.random() * 255);
+};
+
+const generateColor = function generateColor() {
+  document.body.style.background =
+    "rgba(" + randomRGB() + "," + randomRGB() + "," + randomRGB() + ",0.3)";
+};
+
+const changeColor = function changeColor() {
+  if (time.value > 0 && time.value !== null) {
+    lbl.innerHTML = "Background change interval (seconds):";
+    lbl.style.color = "black";
+    warningLabel = false;
+    button.innerHTML = "Stop";
+    button.className = "btn btn-danger";
+    setTimeInterval = setInterval(generateColor, time.value * 1000);
+  } else {
+    if (!warningLabel) {
+      console.log(lbl.innerHTML);
+      lbl.style.color = "red";
+      lbl.innerHTML = lbl.innerHTML + "Plese enter a non-zero positive number!";
+      warningLabel = true;
+    }
+  }
+};
+
+const stopColor = function stopColor() {
+  button.innerHTML = "Start";
+  button.className = "btn btn-primary";
+  clearInterval(setTimeInterval);
+};
+
+document.getElementById("button").addEventListener("click", () => {
+  if (colorChange) {
+    colorChange = false;
+    changeColor();
+  } else {
+    colorChange = true;
+    stopColor();
+  }
+});
