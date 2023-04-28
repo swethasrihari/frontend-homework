9a8976e6c718ae05059b62d30904339fcb0c5092
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-04-28 13:39:12 -0700

    Updated 03-count.js and used foreach iinstead of for loop

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -7,20 +7,14 @@ const regex = new RegExp(searchWord, "gi");
 const findAndHighlight = function findAndHighlight() {
   if (searchWord.value !== "") {
     let words = content.innerHTML.split(" ");
-    for (let i = 0; i < words.length; i++) {
-      if (words[i] === searchWord.value) {
-        words[i] = words[i].replace(
-          /(<mark class="highlight">|<\/mark>)/gim,
-          ""
-        );
-        words[i] = words[i].replace(
-          words[i],
-          '<mark class="highlight">$&</mark>'
-        );
+    words.forEach((element) => {
+      if (element === searchWord.value) {
+        element = element.replace(/(<mark class="highlight">|<\/mark>)/gim, "");
+        element = element.replace(element, '<mark class="highlight">$&</mark>');
         console.log("Highlighting");
       }
       console.log(searchWord.value);
-    }
+    });
     content.innerHTML = words.join(" ");
   } else {
     content.innerHTML = sentence;
