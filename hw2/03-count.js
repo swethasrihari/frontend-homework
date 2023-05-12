f299e8a3d33048216a548473a521afb7c21d1f46
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-05-11 23:04:33 -0700

    count.js updated

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -7,19 +7,23 @@ const regex = new RegExp(searchWord, "gi");
 const findAndHighlight = function findAndHighlight() {
   if (searchWord.value !== "") {
     let words = content.innerHTML.split(" ");
-    words.forEach((element) => {
-      if (element === searchWord.value) {
-        element = element.replace(/(<mark class="highlight">|<\/mark>)/gim, "");
-        element = element.replace(element, '<mark class="highlight">$&</mark>');
-        console.log("Highlighting");
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === searchWord.value) {
+        words[i] = words[i].replace(
+          /(<mark class="highlight">|<\/mark>)/gim,
+          ""
+        );
+        words[i] = words[i].replace(
+          words[i],
+          '<mark class="highlight">$&</mark>'
+        );
       }
       console.log(searchWord.value);
-    });
+    }
     content.innerHTML = words.join(" ");
-  } else {
-    content.innerHTML = sentence;
-  }
+  } else content.innerHTML = sentence;
 };
+
 const enterKeyPressed = function enterKeyPressed(eventNo) {
   if (eventNo.keyCode === 13) {
     content.innerHTML = sentence;
