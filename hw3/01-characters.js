2d4b32dbd3e84fa848aa50d81ae52f1d767ad2f8
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-05-15 11:43:19 -0700

    Added hover and flex to characters.js

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,31 +1,46 @@
 // url for the Thrones API
-
-const display = function display(fullName, title, imageUrl) {
-  document.getElementById(
-    "container"
-  ).innerHTML += `<div  id="col"><p class="fw-bold">${fullName}</p><p>${title}</p></div>`;
-  const img = document.createElement("img");
-  img.src = imageUrl;
-  img.style.width = `200px`;
-  img.style.height = `200px`;
-  /*document.getElementById(
-    "row"
-  ).innerHTML += `<div class="col-md-10 col-lg-2"><img src=imageUrl/></div>`;*/
-  document.getElementById("col").appendChild(img);
-};
-
-const getCharacters = function getCharacters(data) {
-  for (const character of data) {
+const display = function display(data) {
+  const section = document.getElementById('section');
+  section.classList.add(
+    'elements',
+    'd-flex',
+    'flex-wrap',
+    'justify-content-center'
+  );
+  data.forEach((character) => {
     const { fullName, title, imageUrl } = character;
-    //console.log(character.family);
-    display(fullName, title, imageUrl);
-  }
+
+    const characterRow = document.createElement('div');
+    characterRow.classList.add('character');
+
+    const imageRow = document.createElement('div');
+    imageRow.classList.add('image');
+
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.style.width = `225px`;
+    img.style.height = `225px`;
+
+    const nameTitleRow = document.createElement('div');
+    const fullNameH3 = document.createElement('h3');
+    fullNameH3.textContent = fullName;
+    const titleP = document.createElement('p');
+    titleP.textContent = title;
+    nameTitleRow.appendChild(fullNameH3);
+    nameTitleRow.appendChild(titleP);
+    imageRow.appendChild(img);
+    characterRow.appendChild(imageRow);
+    characterRow.append(fullNameH3);
+    characterRow.appendChild(titleP);
+    section.appendChild(characterRow);
+  });
 };
+
 async function fetchData() {
-  const response = await fetch("https://thronesapi.com/api/v2/Characters");
+  const response = await fetch('https://thronesapi.com/api/v2/Characters');
   if (response.status === 200) {
     const data = await response.json();
-    getCharacters(data);
+    display(data);
   }
 }
 
